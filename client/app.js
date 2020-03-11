377a010c8a1f86546cf27b9eec87d9982b999bf1
ellardaaron <59664272+ellardaaron@users.noreply.github.com>
2020-03-11 14:28:28 -0500

    added some styles and added delete feature

--- client/app.js
+++ client/app.js
@@ -1,26 +1,33 @@
 //Load preexisting chirps on page load
-$( document ).ready(function getThemChirps() {
-
-      $.ajax({
-        type: "GET",
-        url: "http://localhost:3000/api/chirps/",
-        data:{ get_param: 'value' },
-        dataType: 'json',
-        success: function (data) { 
-            $.each(data, function(index, element) {
-                $('#chirpDisplay').append($('<div>', {
-                    text: `${element.user} said: ${element.text} at: ${element.timeStamp}`
-                }));
-            });
+$(document).ready(function getThemChirps() {
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3000/api/chirps/",
+    data: { get_param: "value" },
+    dataType: "json",
+    success: function(data) {
+      $.each(data, function(index, element) {
+        if (index !== "nextid") {
+          $("#chirpDisplay").append(`
+          <div class="card">
+            <div class="card-body bg-danger">
+              <h6 class="card-subtitle mb-2 text-white">${element.timeStamp}</h6>
+              <h5 class="card-title">${element.user}</h5>
+              <p class="card-text">${element.text}</p>
+              <button id="deleteChirpButton"  class="btn btn-light" onClick = "deleteChirp(${index})">Delete</button>
+            </div>
+          </div>
+        `);
         }
       });
+    }
+  });
 });
 
 //Post chip when button is clicked
 $("#postChirpButton").click(function() {
-    var chirpText = $("#chirpText").val();
+  var chirpText = $("#chirpText").val();
   var userName = $("#userName").val();
-
 
   $.ajaxSetup({
     headers: {
@@ -41,25 +48,31 @@ $("#postChirpButton").click(function() {
   });
 
   location.reload();
-
 });
+
+// //Function for deleting chirp
+deleteChirp = index => {
+  $.ajax({
+    type: "DELETE",
+    url: `http://localhost:3000/api/chirps/${index}`,
+    success: 200
+  });
+  location.reload();
+};
 
 //Function to get a formatted timestamp
 function timeStamp() {
-    var now = new Date();
-    var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
-    var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
-    var suffix = time[0] < 12 ? "AM" : "PM";
-    time[0] = time[0] < 12 ? time[0] : time[0] - 12;
-    time[0] = time[0] || 12;
-  
-    for (var i = 1; i < 3; i++) {
-      if (time[i] < 10) {
-        time[i] = "0" + time[i];
-      }
+  var now = new Date();
+  var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+  var time = [now.getHours(), now.getMinutes(), now.getSeconds()];
+  var suffix = time[0] < 12 ? "AM" : "PM";
+  time[0] = time[0] < 12 ? time[0] : time[0] - 12;
+  time[0] = time[0] || 12;
+
+  for (var i = 1; i < 3; i++) {
+    if (time[i] < 10) {
+      time[i] = "0" + time[i];
     }
-    return date.join("/") + " " + time.join(":") + " " + suffix;
   }
-
-//Function to get and display chirps
-
+  return date.join("/") + " " + time.join(":") + " " + suffix;
+}
